26e9f326ae4f8fdff8ad5bd57255ae2f8171d5ac
fahim24 <mdtaraquerahmanfahim@gmail.com>
2026-02-07 18:50:52 +0600

    Discount price function is complite

--- newPrice.js
+++ newPrice.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+function newPrice(currentPrice, discount) {
+    if(typeof(currentPrice) === 'number' && typeof(discount) === 'number') {
+        if(0 <= discount && discount <= 100) {
+            return (currentPrice * (1 - discount / 100)).toFixed(3);
+        } else {
+            return "Invalid"
+        }
+    }else {
+        return "Invalid"
+    }
+}
